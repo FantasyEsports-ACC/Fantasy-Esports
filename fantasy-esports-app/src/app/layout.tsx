da429577114600a8e5fc79a60c27f2845301fe6a
Johan <johanlanting04@gmail.com>
2024-10-15 12:52:51 -0500

    Fixed capitalization in NavBar import

--- fantasy-esports-app/src/app/layout.tsx
+++ fantasy-esports-app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css"
 
-import NavBar from "./components/navbar"
+import NavBar from "./components/NavBar"
 
 export const metadata: Metadata = {
   title: "Create Next App",
